ae09537e74911d5c226b9ac3032116f36289bebf
Solomasov Denis <denis.solomasov@renault.com>
2022-03-21 15:42:45 +0300

    add auth code

--- app/src/main/java/com/example/myapplication/AuthenticationFragmentViewModel.kt
+++ app/src/main/java/com/example/myapplication/AuthenticationFragmentViewModel.kt
@@ -1,20 +1,51 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.net.Uri
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.myapplication.configuration.ApplicationConfig
+import com.example.myapplication.configuration.ApplicationConfigLoader
+import net.openid.appauth.AuthorizationServiceConfiguration
+import net.openid.appauth.ResponseTypeValues
+import net.openid.appauth.AuthorizationRequest
+import androidx.core.app.ActivityCompat.startActivityForResult
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import net.openid.appauth.AuthorizationService
 
-class AuthenticationFragmentViewModel: ViewModel() {
+class AuthenticationFragmentViewModel(context: Context): ViewModel() {
 
     val TAG = "RenaultAuth"
     var error = ""
+    val config: ApplicationConfig = ApplicationConfigLoader().load(context)
 
-    fun startLogin() {
+    fun startLogin(context: Context) {
 
         error = ""
 
         Log.d(TAG, "start login...")
 
+        val serviceConfig = AuthorizationServiceConfiguration(
+            Uri.parse(config.auth_uri),
+            Uri.parse(config.token_uri)
+        )
 
+        val authRequestBuilder = AuthorizationRequest.Builder(
+            serviceConfig,  // the authorization service configuration
+            config.clientID,  // the client ID, typically pre-registered and static
+            ResponseTypeValues.CODE,  // the response_type value: we want a code
+            Uri.parse(config.redirectUri)
+        )
+
+        val authRequest = authRequestBuilder
+            .setScope("openid email")
+            //.setLoginHint("jdoe@user.example.com")
+            .build();
+
+        val authService = AuthorizationService(context)
+        val authIntent = authService.getAuthorizationRequestIntent(authRequest)
 
     }
 
--- app/src/main/java/com/example/myapplication/configuration/ApplicationConfig.kt
+++ app/src/main/java/com/example/myapplication/configuration/ApplicationConfig.kt
@@ -4,4 +4,6 @@ class ApplicationConfig {
     lateinit var clientID: String
     lateinit var redirectUri: String
     lateinit var scope: String
+    lateinit var auth_uri: String
+    lateinit var token_uri: String
 }
